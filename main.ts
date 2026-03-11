7e955daa4b8044071c48376f028cefde1c56af06
murali-kothuri <murali.kothuri@stu.ocsb.ca>
2026-03-11 13:03:54 -0400

    change and commited

--- main.ts
+++ main.ts
@@ -20,23 +20,24 @@ input.onGesture(Gesture.Shake, function () {
     // if randomNumber was 0
     if (randomNumber == 0) {
         basic.showIcon(IconNames.SmallSquare)
+        basic.pause(5000)
         basic.showIcon(IconNames.Happy)
     }
 
     // if randomNumber was 1
     if (randomNumber == 1) {
         basic.showIcon(IconNames.Square)
+        basic.pause(5000)
         basic.showIcon(IconNames.Happy)
     }
 
     // if randomNumber was 2
     if (randomNumber == 2) {
         basic.showIcon(IconNames.Scissors)
+        basic.pause(5000)
         basic.showIcon(IconNames.Happy)
     }
 
-    // hold the icon for 5 seconds
-    basic.pause(5000)
     basic.clearScreen()
 })
 
