5857a60d11fba7c45523e7f67e0c957c64d152c5
murali-kothuri <murali.kothuri@stu.ocsb.ca>
2026-03-11 12:55:09 -0400

    change and commited

--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Murali Kothuri
+ * Created on: Feb 2026
+ * This program is a rock, paper, scissors game module.
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber = (0,2)
+let score = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // if randomNumber was 0
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+
+    // if randomNumber was 1
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    // if randomNumber was 2
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
+
+    // hold the icon for 5 seconds
+    basic.pause(5000)
+    basic.clearScreen()
+})
+
+// press A to increase score
+input.onButtonPressed(Button.A, function() {
+    score = score + 1
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+
+// press B to check the current score
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("Score:" + score)
+})
